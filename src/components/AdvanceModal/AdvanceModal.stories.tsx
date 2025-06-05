6e3a5d3da68433db1e4b11174afff26d8f38de57
Nitish Kumar <150783174+whoisni30@users.noreply.github.com>
2025-06-05 23:39:51 +0530

    Create AdvanceModal.stories.tsx

---
+++ src/components/AdvanceModal/AdvanceModal.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdvancedModal from './AdvancedModal';
+
+export default {
+  title: 'Components/AdvancedModal',
+  component: AdvancedModal,
+};
+
+export const BasicModal = () => <AdvancedModal />;
+
+// 📁 src/react-app-env.d.ts
+/// <reference types="react-scripts" />
