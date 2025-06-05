d9fafb3654e01f285b8bf30b1e6b430c5d6d7b9e
Nitish Kumar <150783174+whoisni30@users.noreply.github.com>
2025-06-05 22:54:27 +0530

    Create themes.ts

---
+++ src/theme/themes.ts
@@ -0,0 +1,9 @@
+export type Theme = "light" | "dark";
+
+export function applyTheme(theme: Theme) {
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}
