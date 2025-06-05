541f91890304c0c7e62c17b54d505b17b955ed2a
Nitish Kumar <150783174+whoisni30@users.noreply.github.com>
2025-06-05 22:44:46 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./.storybook/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // for light/dark toggling
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)',
+        success: 'var(--color-success)',
+        info: 'var(--color-info)',
+        warning: 'var(--color-warning)',
+        error: 'var(--color-error)',
+        neutral: {
+          100: 'var(--color-neutral-100)',
+          200: 'var(--color-neutral-200)',
+          300: 'var(--color-neutral-300)',
+          400: 'var(--color-neutral-400)',
+          500: 'var(--color-neutral-500)',
+          600: 'var(--color-neutral-600)',
+          700: 'var(--color-neutral-700)',
+          800: 'var(--color-neutral-800)',
+          900: 'var(--color-neutral-900)',
+        },
+      }
+    }
+  },
+  plugins: [],
+}
